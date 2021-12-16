b85639466ab3dfd83f3d560f2655716a8ebd06fe
Pacy Wu <peihsi5@uw.edu>
2021-12-15 20:30:13 -0800

    add recipe to folder

--- lemmeShoyu/app/src/main/java/edu/uw/peihsi5/lemmeshoyu/RecipeDetailFragment.kt
+++ lemmeShoyu/app/src/main/java/edu/uw/peihsi5/lemmeshoyu/RecipeDetailFragment.kt
@@ -5,13 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import edu.uw.peihsi5.lemmeshoyu.dialogs.ChooseFolderDialogFragment
 import edu.uw.peihsi5.lemmeshoyu.network.Recipe
 
 
@@ -40,7 +39,8 @@ class RecipeDetailFragment : Fragment() {
         val recipePhoto: ImageView = rootView.findViewById<ImageView>(R.id.recipe_photo)
         Glide.with(rootView).load(recipe!!.imagePath).into(recipePhoto)
         rootView.findViewById<FloatingActionButton>(R.id.add_recipe_to_folder_button).setOnClickListener{
-            //TODO
+            val dialog = ChooseFolderDialogFragment(recipe!!)
+            dialog.show(requireActivity().supportFragmentManager, "Choose Folder Dialog")
         }
         return rootView
     }
