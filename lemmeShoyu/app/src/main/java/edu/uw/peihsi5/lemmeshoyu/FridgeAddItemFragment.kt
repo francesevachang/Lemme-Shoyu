5e561db54f12df0cbbef210eda001f90ca6cb21f
fran12 <fran12@uw.edu>
2021-12-15 17:33:47 -0800

    commit

--- lemmeShoyu/app/src/main/java/edu/uw/peihsi5/lemmeshoyu/FridgeAddItemFragment.kt
+++ lemmeShoyu/app/src/main/java/edu/uw/peihsi5/lemmeshoyu/FridgeAddItemFragment.kt
@@ -1,27 +1,78 @@
-package edu.uw.peihsi5.lemmeshoyu
+package edu.uw.peihsi5.lemmeshoyu.dialogs
 
+import edu.uw.peihsi5.lemmeshoyu.R
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
+import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+import edu.uw.peihsi5.lemmeshoyu.database.Folder
+import edu.uw.peihsi5.lemmeshoyu.database.my_fridge_database.Ingredient
+import edu.uw.peihsi5.lemmeshoyu.viewmodels.FolderViewModel
+import edu.uw.peihsi5.lemmeshoyu.viewmodels.MyFridgeViewModel
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FridgeAddItemFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class FridgeAddItemFragment : Fragment() {
+private const val TAG = "FridgeAddItemDialogFragment"
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+class FridgeAddItemFragment(): DialogFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val rootView = inflater.inflate(R.layout.fragment_fridge_add_item, container, false)
+
+        val closeButton = rootView.findViewById<ImageButton>(R.id.fridge_add_item_dialog_close_btn)
+        closeButton.setOnClickListener {
+            this.dismiss()
+        }
+
+        val doneButton = rootView.findViewById<Button>(R.id.fridge_add_item_dialog_done_btn)
+        doneButton.setOnClickListener {
+            val itemName = rootView.findViewById<TextInputEditText>(R.id.fridge_add_item_dialog_text1).text.toString().trim()
+            val expDate = rootView.findViewById<TextInputEditText>(R.id.fridge_add_item_dialog_text2).text.toString().trim()
+            if (itemName.isNotEmpty() && expDate.isNotEmpty()) { // TODO: error handling of date format
+                val viewModel = ViewModelProvider(
+                    this,
+                    ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application)
+                ).get(MyFridgeViewModel::class.java)
+
+                viewModel.insert(
+                    Ingredient(
+                        itemName = itemName,
+                        expireMonth = expDate.substring(0, 2).toInt(),
+                        expireDay = expDate.substring(3, 5).toInt(),
+                        expireYear = expDate.substring(6).toInt())
+                ) {
+//                    this.insertExceptionHandler()
+                }
+
+                this.dismiss()
+            } else {
+                val errorTextView = rootView.findViewById<TextView>(R.id.fridge_add_item_dialog_error_msg)
+                errorTextView.visibility = View.VISIBLE
+            }
+
+        }
+
+        return rootView
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fridge_add_item, container, false)
-    }
+//    private fun insertExceptionHandler() {
+//        Handler(Looper.getMainLooper()).post {
+//            val root = requireActivity().findViewById<RelativeLayout>(R.id.root_activity_main)
+//            Snackbar.make(root, "The folder name already exists. Please enter another folder name", Snackbar.LENGTH_LONG)
+//                .show()
+//        }
+//    }
+
+
+
+
+
+
 }
